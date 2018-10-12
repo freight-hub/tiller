6f033fe3c5d719b9462c65b2a85529cd19e356b7
serhii-dolia <serhii.dolia@freighthub.com>
2018-10-12 15:11:19 +0200

    added mongodb-3 support

--- src/Collection.ts
+++ src/Collection.ts
@@ -1,17 +1,17 @@
-import {DB} from "./DB";
-import {fromDB} from "./core";
-import {Document} from "./Document";
+import { DB } from "./DB";
+import { fromDB } from "./core";
+import { Document } from "./Document";
 import * as mongodb from 'mongodb';
-import {InsertOneWriteOpResult} from  "mongodb";
-import {ValidationError} from "./decorators/validate";
+import { InsertOneWriteOpResult } from "mongodb";
+import { ValidationError } from "./decorators/validate";
 import * as Bluebird from 'bluebird';
 
 export abstract class Collection extends Document {
-    _id:any
-    __isSaved:boolean
+    _id: any
+    __isSaved: boolean
 
-    static _collectionName:string;
-    static __type:Function;
+    static _collectionName: string;
+    static __type: Function;
 
     constructor() {
         super();
@@ -21,7 +21,7 @@ export abstract class Collection extends Document {
         })
     }
 
-    static async create<Type extends Collection>(obj:any):Promise<Type> {
+    static async create<Type extends Collection>(obj: any): Promise<Type> {
         let type = this.__type || (this._collectionName ? this : null);
         return fromDB<Type>(type, obj);
     }
@@ -34,8 +34,8 @@ export abstract class Collection extends Document {
      */
     async updateProperties<Type extends Collection>(obj) {
         let doc = await fromDB(this.constructor, obj);
-        for(var key of Object.keys(doc)) {
-            if(key.indexOf('__') == 0) {
+        for (var key of Object.keys(doc)) {
+            if (key.indexOf('__') == 0) {
                 continue;
             }
             this[key] = doc[key];
@@ -43,19 +43,19 @@ export abstract class Collection extends Document {
         return this;
     }
 
-    static async get<Type extends Collection>(_id:any):Promise<Type> {
-        return this.findOne<Type>({_id: _id});
+    static async get<Type extends Collection>(_id: any): Promise<Type> {
+        return this.findOne<Type>({ _id: _id });
     }
 
-    static async findOne<Type extends Collection>(selector:any):Promise<Type> {
+    static async findOne<Type extends Collection>(selector: any): Promise<Type> {
         return (await this.find<Type>(selector, 1))[0];
     }
 
-    static async find<Type extends Collection>(selector:any, limit?:number, sort?:any):Promise<Array<Type>> {
+    static async find<Type extends Collection>(selector: any, limit?: number, sort?: any): Promise<Array<Type>> {
         let type = this.__type || (this._collectionName ? this : null);
         let coll = await DB.collection((<any>type)._collectionName);
         let cursor = coll.find(selector);
-        if (typeof(limit) == 'number') {
+        if (typeof (limit) == 'number') {
             cursor = cursor.limit(limit);
         }
         if (sort) {
@@ -69,11 +69,11 @@ export abstract class Collection extends Document {
         });
     }
 
-    static async all<Type extends Collection>():Promise<Array<Type>> {
+    static async all<Type extends Collection>(): Promise<Array<Type>> {
         return (await this.find<Type>({}));
     }
 
-    static async count(selector?:any):Promise<number> {
+    static async count(selector?: any): Promise<number> {
         let type = this.__type || (this._collectionName ? this : null);
         let coll = await DB.collection((<any>type)._collectionName);
         let cursor = coll.find(selector || {});
@@ -88,7 +88,7 @@ export abstract class Collection extends Document {
      *  with `new`, but the _id already exists in the database.
      * @returns {Collection}
      */
-    async save(deep?:boolean, upsert?:boolean) {
+    async save(deep?: boolean, upsert?: boolean) {
         let validation = await this.validate();
         if (!validation.valid()) {
             throw new ValidationError(this.constructor.name + (this.isSaved() ? '#' + this._id : '') + ' is not valid: ' + validation.toString(), validation);
@@ -100,13 +100,13 @@ export abstract class Collection extends Document {
         let coll = await DB.collection(collectionName)
         let doc = await this.toDB(deep)
         if (this.isNew() && !upsert) {
-            let result:InsertOneWriteOpResult = await coll.insertOne(doc)
+            let result: InsertOneWriteOpResult = await coll.insertOne(doc)
             this._id = result.insertedId;
         } else {
             if (!this._id) {
                 throw new Error('To update or upsert a document an _id is required');
             }
-            await coll.updateOne({_id: this._id}, doc, {upsert: upsert});
+            await coll.replaceOne({ _id: this._id }, doc, { upsert: upsert });
         }
 
         await this.afterSave();
@@ -122,7 +122,7 @@ export abstract class Collection extends Document {
      * @param deep
      * @returns {Promise<Collection>}
      */
-    async upsert(deep?:boolean) {
+    async upsert(deep?: boolean) {
         return this.save(deep, true);
     }
 
@@ -135,7 +135,7 @@ export abstract class Collection extends Document {
 
         let collectionName = (<any>this)._collectionName;
         let coll = await DB.collection(collectionName);
-        await coll.deleteOne({_id: this._id});
+        await coll.deleteOne({ _id: this._id });
 
         await this.afterDestroy()
     }
@@ -155,11 +155,11 @@ export abstract class Collection extends Document {
         return this.__isSaved;
     }
 
-    _collectionName():string {
+    _collectionName(): string {
         return (<any>this)._collectionName;
     }
 
-    async _collection():Promise < mongodb.Collection > {
+    async _collection(): Promise<mongodb.Collection> {
         return DB.collection(this._collectionName());
     }
 
